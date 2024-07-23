d4863db3309160872053fd2cae6fafa2ed9c0d40
Akhila Vanaja Anand <vanajaananda@cardiff.ac.uk>
2024-07-23 06:56:28 +0100

    Set up routing in App.js

--- frontend/health-tracking-app/src/App.js
+++ frontend/health-tracking-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import SummaryChart from './SummaryChart';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <h1>Health Tracking App</h1>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/summary" element={<SummaryChart />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
+
